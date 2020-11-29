5341574ab36f17c9af5f83e9db5d81f8b3be0df2
SMKrizan <smkrizan@gmail.com>
2020-11-28 23:31:10 -0600

    database connects

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+require('dotenv').config({ path: '../.env' });
+
+const Sequelize = require('sequelize');
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_PWD);
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+      // dialectOptions: {
+      //   decimalNumbers: true,
+      // },
+    });
+
+module.exports = sequelize;
